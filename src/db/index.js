ce340f8535b7dbced1b6c490ab429db981cce54d
Deepak kumar <dee96872@gmail.com>
2024-09-15 15:27:50 +0530

    add some routes

--- src/db/index.js
+++ src/db/index.js
@@ -1,3 +1,4 @@
+
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
@@ -5,11 +6,11 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log("MONGODB connection FALED", error)
+        console.log("MONGODB connection FAILED ", error);
         process.exit(1)
     }
 }
 
-export default connectDB;
+export default connectDB
